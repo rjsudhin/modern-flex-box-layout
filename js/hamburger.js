704192c0dc06c0342c43abc854ee0d53f82d3ad9
SudhinRj <rjsudhin@users.noreply.github.com>
2025-07-30 01:05:30 +0530

    hamburger is not working and clickable

---
+++ js/hamburger.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector('#hamburger')
+const menu = document.querySelector('.menu')
+
+
+
+
+hamburger.addEventListener('click', () => {
+  menu.classList.toggle('active')
+})
